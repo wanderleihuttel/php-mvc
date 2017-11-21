0e937c2675874a4354bdd0ae57a83afebf00a4d6
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-11-20 23:32:45 -0200

    Modify app/views

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,4 +1,6 @@
 <?php require APP_ROOT . '/views/inc/header.php' ?>
-<?php echo $data['title']; ?>
-<?php echo APP_ROOT; ?>
+<h1><?php echo $data['title']; ?></h1>
+<p><?php echo $data['description']; ?></p>
+<p>Version: <strong><?php echo APP_VERSION; ?></strong>
+<p>Date: <strong><?php echo APP_DATE; ?></strong>
 <?php require APP_ROOT . '/views/inc/footer.php' ?>
---
+++ app/views/users/register.php
@@ -0,0 +1,43 @@
+<?php require APP_ROOT . '/views/inc/header.php' ?>
+<div class="row">
+   <div class="col-md-6 mx-auto">
+      <div class="card card-body bg-light mt-5">
+         <h3>Create an account</h3>
+         <p>Please fill out this form to register with us</p>
+         <form action="<?php echo URL_ROOT; ?>/users/register" method="post">
+            <div class="form-group">
+               <label for="name">Name: <sup>*</sup></label>
+               <input type="text" name="name" class="form-control form-control <?php echo (!empty($data['name_err'])) ? 'is-invalid' : '';?>" value="<?php echo $data['name']; ?>">
+               <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
+            </div>
+            <div class="form-group">
+               <label for="name">Email: <sup>*</sup></label>
+               <input type="email" name="email" class="form-control form-control <?php echo (!empty($data['email_err'])) ? 'is-invalid' : '';?>" value="<?php echo $data['email']; ?>">
+               <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+            </div>
+            <div class="form-group">
+               <label for="name">Password: <sup>*</sup></label>
+               <input type="password" name="password" class="form-control form-control <?php echo (!empty($data['password_err'])) ? 'is-invalid' : '';?>" value="<?php echo $data['password']; ?>">
+               <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+            </div>
+            <div class="form-group">
+               <label for="name">Confirm Password: <sup>*</sup></label>
+               <input type="password" name="confirm_password" class="form-control form-control <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : '';?>" value="<?php echo $data['confirm_password']; ?>">
+               <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
+            </div>
+            
+            <div class="row">
+               <div class="col">
+                  <input type="submit" value="Register" class="btn btn-success btn-block"/>
+               </div>
+               <div class="col">
+                  <a href="<?php echo URL_ROOT; ?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
+               </div>
+               
+            </div>
+            
+         </form>
+         
+   </div>
+</div>
+<?php require APP_ROOT . '/views/inc/footer.php' ?>
