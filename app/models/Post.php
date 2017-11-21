914dde6fdfa781d1c07dbaafbf3ea45672129753
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-11-21 18:49:46 -0200

    Modify models

--- app/models/Post.php
+++ app/models/Post.php
@@ -1,17 +1,75 @@
 <?php
+
+
     class Post
     {
        private $db;
+
        public function __construct()
        {
-          $this->db = new Database();
+           $this->db = new Database();
        }
-
 
        public function getPosts()
        {
-          $this->db->query('select * from posts');
-          return $this->db->resultSet();
+           $this->db->query('select p.id as post_id, p.user_id, u.name, u.email, p.title, p.body, p.created_at 
+                                 from posts p 
+                                 left join users u on u.id = p.user_id 
+                                 order by p.created_at desc');
+           return $this->db->resultSet();
 
+       }
+
+       public function getPostById($id)
+       {
+           $this->db->query('select * from posts where id = :id');
+           $this->db->bind(':id',$id);
+           return $this->db->single();
+       }
+
+       public function addPost($data)
+       {
+           $this->db->query('INSERT INTO posts (user_id, title, body) values (:user_id, :title, :body)');
+           // Bind values
+           $this->db->bind(':user_id', $data['user_id']);
+           $this->db->bind(':title', $data['title']);
+           $this->db->bind(':body', $data['body']);
+
+           // Execute
+           if( $this->db->execute() ){
+               return true;
+           } else {
+               return false;
+           }
+       }
+
+       public function updatePost($data)
+       {
+           $this->db->query('UPDATE posts SET title = :title, body = :body where id = :id');
+           // Bind values
+           $this->db->bind(':id', $data['id']);
+           $this->db->bind(':title', $data['title']);
+           $this->db->bind(':body', $data['body']);
+
+           // Execute
+           if( $this->db->execute() ){
+               return true;
+           } else {
+               return false;
+           }
+       }
+
+       public function deletePost($id)
+       {
+           $this->db->query('DELETE FROM posts where id = :id');
+           // Bind values
+           $this->db->bind(':id', $id);
+
+           // Execute
+           if( $this->db->execute() ){
+               return true;
+           } else {
+               return false;
+           }
        }
     }
--- app/models/User.php
+++ app/models/User.php
@@ -1,58 +1,63 @@
 <?php
 
-class User{
-   private $db;
-   
-   public function __construct()
-   {
-      $this->db = new Database();
-   }
-   
-   public function register($data)
-   {
-      $this->db->query('INSERT INTO users (name, email, password) values (:name, :email, :password)');
-      // Bind values
-      $this->db->bind(':name', $data['name']);
-      $this->db->bind(':email', $data['email']);
-      $this->db->bind(':password', $data['password']);
-      
-      // Execute
-      if( $this->db->execute() ){
-         return true;
-      } else {
-         return false;
-      }
-   }
-   
-   public function login($email,$password)
-   {
-      $this->db->query('SELECT * from users where email = :email');
-      $this->db->bind(':email', $email);
-      $row = $this->db->single();
-      
-      $hashed_password = $row->password;
-      if( password_verify($password,$hashed_password) ){
-         return $row;
-      } else {
-         return false;
-      }
-   }
-   
-   public function findUserByEmail($email)
-   {
-      $this->db->query('SELECT * FROM users WHERE email = :email');
-      // Bind values
-      $this->db->bind(':email', $email);
-      $row = $this->db->single();
-      
-      // Check row
-      if( $this->db->rowCount() > 0 ){
-         return true;
-      } else{
-         return false;
-      }
-   }
-   
-   
-   
-}
+    class User{
+
+        private $db;
+
+        public function __construct()
+        {
+          $this->db = new Database();
+        }
+
+        public function register($data)
+        {
+            $this->db->query('INSERT INTO users (name, email, password) values (:name, :email, :password)');
+            // Bind values
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':email', $data['email']);
+            $this->db->bind(':password', $data['password']);
+            // Execute
+            if( $this->db->execute() ){
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public function login($email,$password)
+        {
+            $this->db->query('SELECT * from users where email = :email');
+            $this->db->bind(':email', $email);
+            $row = $this->db->single();
+
+            $hashed_password = $row->password;
+            if( password_verify($password,$hashed_password) ){
+                return $row;
+            } else {
+                return false;
+            }
+        }
+
+        public function getUserByEmail($email)
+        {
+            $this->db->query('SELECT * FROM users WHERE email = :email');
+            // Bind values
+            $this->db->bind(':email', $email);
+            $this->db->single();
+
+            // Check row
+            if( $this->db->rowCount() > 0 ){
+                return true;
+            } else{
+                return false;
+            }
+        }
+
+        public function getUserById($id)
+        {
+            $this->db->query('SELECT * FROM users WHERE id = :id');
+            // Bind values
+            $this->db->bind(':id', $id);
+            return $this->db->single();
+        }
+    }
