f494c1fc5c10cdb888fd458e49b163286fd49a47
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-11-21 18:50:54 -0200

    Modify controllers

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,31 +1,27 @@
 <?php
 
 
-class Pages extends  Controller
-{
+    class Pages extends Controller
+    {
 
-   public function __construct() {
-      //$this->postModel = $this->model('Post');
-   }
+       public function index()
+       {
+          if( isLoggedIn() ) {
+             redirect('posts');
+          }
+          $data = [
+              'title' => 'PHP MVC Framework',
+              'description' => 'Simple social network built using PHP/MVC.'
+          ];
+          $this->view('pages/index', $data);
+       }
 
-   public function index()
-   {
-      //$posts = $this->postModel->getPosts();
-      $data = [
-         'title' => 'PHP MVC Framework',
-         'description' => 'Simple social network built using PHP/MVC.'
-      ];
-
-      $this->view('pages/index', $data);
-   }
-
-
-   public function about()
-   {
-      $data = [
-         'title' => 'About Us',
-         'description' => 'App to share posts with other users'
-      ];
-      $this->view('pages/about',$data);
-   }
-}
+       public function about()
+       {
+          $data = [
+              'title' => 'About Us',
+              'description' => 'App to share posts with other users'
+          ];
+          $this->view('pages/about',$data);
+       }
+    }
