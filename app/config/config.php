901a06276161d8b009caf573c8cf19b823f45ce6
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-11-21 18:49:27 -0200

    Modify config

--- app/config/config.php
+++ app/config/config.php
@@ -1,20 +1,22 @@
 <?php
 
-   // Database Params
-   define('DB_HOST','localhost');
-   define('DB_USER','mvc');
-   define('DB_PASS','mvc123');
-   define('DB_NAME','mvc');
-   define('DB_CHARSET','utf8');
+    // Database Params
+    define('DB_HOST','localhost');
+    define('DB_USER','mvc');
+    define('DB_PASS','mvc123');
+    define('DB_NAME','mvc');
+    define('DB_CHARSET','utf8');
 
-   // App Root
-   define('APP_ROOT', dirname(dirname(__FILE__)));
-   // URL Root
-   define('URL_ROOT','http://192.168.1.1/mvc');
-   // Site Name
-   define('SITE_NAME', 'MVC/PHP');
-   // App Version
-   define('APP_VERSION', '1.0.0');
-   define('APP_DATE', 'NOV 20, 2017');
+    // App Root
+    define('APP_ROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URL_ROOT','http://192.168.1.1/mvc');
+    // Site Name
+    define('SITE_NAME', 'MVC/PHP');
+    // App Version
+    define('APP_VERSION', '1.0.0');
+    define('APP_DATE', 'NOV 20, 2017');
+
+    define('APP_DATE_TIME_FORMAT', 'd/m/Y H:i:s');
 
 
