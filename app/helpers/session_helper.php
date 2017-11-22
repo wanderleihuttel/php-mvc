a4045b193bd45c30e23318e8dc2a3885c1609c2d
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-11-21 22:24:41 -0200

    Modify flash message to allow dismiss

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -4,7 +4,7 @@
     // Flash message helper
     // Example flash('register_success','You are now registered!')
     // Display in View - <php echo flash('register_success'); >
-    function flash($name = '', $message = '', $class = 'alert alert-success')
+    function flash($name = '', $message = '', $class = 'alert alert-success alert-dismissible fade show')
     {
         if (! empty($name) ) {
             if (! empty($message) && empty($_SESSION['name']) ) {
@@ -18,7 +18,11 @@
                 $_SESSION[$name. '_class'] = $class;
             } elseif ( empty($mesage) && !empty($_SESSION[$name]) ) {
                 $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
-                echo '<div class="'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
+                echo '<div class="'.$class.'" id="msg-flash" role="alert">' . $_SESSION[$name] . '
+                         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                         <span aria-hidden="true">&times;</span></button>
+                     </div>';
+                
                 unset($_SESSION[$name]);
                 unset($_SESSION[$name.'_class']);
             }
