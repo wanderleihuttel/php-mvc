8d256f02f834a24f456cdfb0b4b6e35870c595f4
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2017-11-23 16:44:12 -0200

    Modify navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -13,13 +13,15 @@
          <li class="nav-item">
              <a class="nav-link" href="<?php echo URL_ROOT;?>/users">Users</a>
          </li>
-         <li class="nav-item">
-            <a class="nav-link" href="<?php echo URL_ROOT;?>/users/logout">Logout</a>
-         </li>
           <?php endif; ?>
           <li class="nav-item">
               <a class="nav-link" href="<?php echo URL_ROOT;?>/pages/about">About</a>
           </li>
+          <?php if( isset($_SESSION['user_id'])) : ?>
+              <li class="nav-item">
+                  <a class="nav-link" href="<?php echo URL_ROOT;?>/users/logout">Logout</a>
+              </li>
+          <?php endif; ?>
       </ul>
    
       <ul class="navbar-nav mr-right">
